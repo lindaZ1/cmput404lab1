7a84ada91d80bc50aeb734b463ec9bb9a8b3f959
lindaZ1 <dzhang4@ualberta.ca>
2016-09-07 15:16:27 -0600

    made a change

--- lab1.py
+++ lab1.py
@@ -1,3 +1,3 @@
 import requests
-response=requests.post('http://ccid-eddieantonio.rhcloud.com/dzhang4')
-print(response.status_code)
+response=requests.get('https://github.com/lindaZ1/cmput404lab1/raw/master/lab1.py')
+print(response.text)
